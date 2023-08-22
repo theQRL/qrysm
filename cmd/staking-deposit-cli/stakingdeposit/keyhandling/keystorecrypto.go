b971f29e57a1799b31bef103ae81bb2d5b56510e
cyyber <kaushal.forex@gmail.com>
2023-08-22 07:54:32 +0530

    Fixed initialization and variable name of KeystoreCrypto

--- cmd/staking-deposit-cli/stakingdeposit/keyhandling/keystorecrypto.go
+++ cmd/staking-deposit-cli/stakingdeposit/keyhandling/keystorecrypto.go
@@ -1,15 +1,14 @@
 package keyhandling
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
 
 type KeystoreCrypto struct {
-	kdf      *KeystoreModule
-	checksum *KeystoreModule
-	cipher   *KeystoreModule
-}
-
-func NewKeystoreCryptoFromJSON() *KeystoreCrypto {
-	return nil
+	KDF      *KeystoreModule `json:"kdf"`
+	Checksum *KeystoreModule `json:"checksum"`
+	Cipher   *KeystoreModule `json:"cipher"`
 }
 
 func CheckSumDecryptionKeyAndMessage(partialDecryptionKey, cipherText []uint8) [32]byte {
@@ -23,15 +22,32 @@ func NewKeystoreCrypto(salt, aesIV, cipherText, partialDecryptionKey []uint8) *K
 	checksum := CheckSumDecryptionKeyAndMessage(partialDecryptionKey, cipherText)
 
 	return &KeystoreCrypto{
-		kdf: &KeystoreModule{
-			params: map[string]interface{}{"salt": salt},
+		KDF: &KeystoreModule{
+			Params: map[string]interface{}{"salt": hex.EncodeToString(salt)},
 		},
-		cipher: &KeystoreModule{
-			params:  map[string]interface{}{"iv": aesIV},
-			message: cipherText,
+		Cipher: &KeystoreModule{
+			Function: "aes-128-ctr",
+			Params:   map[string]interface{}{"iv": hex.EncodeToString(aesIV)},
+			Message:  hex.EncodeToString(cipherText),
 		},
-		checksum: &KeystoreModule{
-			message: checksum[:],
+		Checksum: &KeystoreModule{
+			Function: "sha256",
+			Params:   map[string]interface{}{},
+			Message:  hex.EncodeToString(checksum[:]),
+		},
+	}
+}
+
+func NewEmptyKeystoreCrypto() *KeystoreCrypto {
+	return &KeystoreCrypto{
+		KDF: &KeystoreModule{
+			Params: map[string]interface{}{},
+		},
+		Cipher: &KeystoreModule{
+			Params: map[string]interface{}{},
+		},
+		Checksum: &KeystoreModule{
+			Params: map[string]interface{}{},
 		},
 	}
 }
