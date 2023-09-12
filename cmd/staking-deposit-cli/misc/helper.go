a0f28293894df53650132134ee462e41b3d33652
cyyber <kaushal.forex@gmail.com>
2023-09-12 13:10:17 +0530

    Added helper functions DecodeHex, ToSizedDilithiumSignature & ToSizedDilithiumPublicKey

--- cmd/staking-deposit-cli/misc/helper.go
+++ cmd/staking-deposit-cli/misc/helper.go
@@ -2,7 +2,10 @@ package misc
 
 import (
 	"encoding/hex"
+	"fmt"
+
 	"github.com/theQRL/go-qrllib/common"
+	dilithium2 "github.com/theQRL/go-qrllib/dilithium"
 )
 
 func StrSeedToBinSeed(strSeed string) [common.SeedSize]uint8 {
@@ -15,4 +18,31 @@ func StrSeedToBinSeed(strSeed string) [common.SeedSize]uint8 {
 
 	copy(seed[:], unSizedSeed)
 	return seed
+}
+
+func DecodeHex(hexString string) []byte {
+	hexBytes, err := hex.DecodeString(hexString)
+	if err != nil {
+		panic(fmt.Errorf("failed to decode string %s | reason %v",
+			hexString, err))
+	}
+	return hexBytes
+}
+
+func ToSizedDilithiumSignature(sig []byte) [dilithium2.CryptoBytes]byte {
+	if len(sig) != dilithium2.CryptoBytes {
+		panic(fmt.Errorf("cannot convert sig to sized dilithium sig, invalid sig length %d", len(sig)))
+	}
+	var sizedSig [dilithium2.CryptoBytes]byte
+	copy(sizedSig[:], sig)
+	return sizedSig
+}
+
+func ToSizedDilithiumPublicKey(pk []byte) [dilithium2.CryptoPublicKeyBytes]byte {
+	if len(pk) != dilithium2.CryptoPublicKeyBytes {
+		panic(fmt.Errorf("cannot convert pk to sized dilithium pk, invalid pk length %d", len(pk)))
+	}
+	var sizedPK [dilithium2.CryptoPublicKeyBytes]byte
+	copy(sizedPK[:], pk)
+	return sizedPK
 }
